23e872cd2e8ffec29adf8e8fcb1be79beefd5c8f
srinivask <srinivas.kalam@imaginea.com>
2016-11-02 16:26:10 +0530

    minor change to sql

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,21 +1,23 @@
 DROP TABLE IF EXISTS address;
+
+CREATE TABLE address (
+	id serial PRIMARY KEY NOT NULL,
+	street varchar(20) NOT NULL,
+	zip int NOT NULL
+);
+
 DROP TABLE IF EXISTS customer;
- 
+
 CREATE TABLE customer (
     CUST_ID serial PRIMARY KEY NOT NULL,
     NAME varchar(100) NOT NULL,
-    AGE smallint NOT NULL
+    AGE smallint NOT NULL,
+    address_id int DEFAULT NULL REFERENCES address(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 INSERT INTO customer values (1, 'srinivas', 26);
 INSERT INTO customer values (2, 'pranay', 30);
 INSERT INTO customer values (3, 'bhanu', 20);
-
-CREATE TABLE address (
-	id serial PRIMARY KEY NOT NULL REFERENCES customer(cust_id) ON DELETE CASCADE,
-	street varchar(20) NOT NULL,
-	zip int NOT NULL
-);
 
 INSERT INTO address values (1, 'judges colony', 1070);
 INSERT INTO address values (2, 'kphb', 1071);
